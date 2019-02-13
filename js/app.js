f86dadd7044b402081c2c25655adb25a6b345cd1
Shannan Kammann <sbkammann@gmail.com>
2019-02-13 14:28:47 -0800

    feat: Added button depress css

--- js/app.js
+++ js/app.js
@@ -11,6 +11,25 @@ const D = document.getElementById('D');
 const Z = document.getElementById('Z');
 const X = document.getElementById('X');
 const C = document.getElementById('C');
-const keyBox =  document.getElementById('keyBox');
 
-keyBox.addEventListener('click', (event)=> Q.play()); //console.log(event.target.firstElementChild.id)
+const keyBox =  document.getElementById('keyBox');
+const playObj = {Q, W, E, A, S, D, Z, X, C};
+
+keyBox.addEventListener('click', event => playObj[event.target.firstElementChild.id].play()); //console.log(event.target.firstElementChild.id)
+window.addEventListener('keypress', buttonPress.bind(this, event) ); //playObj[String.fromCharCode(event.keyCode).toUpperCase()].play()
+
+// var alertText = function(text) {
+// var event = this.event;
+// alert(text);
+// };
+
+//document.getElementById('someelem').addEventListener('click', alertText.bind(this, 'hello'));
+function buttonPress(event){
+  // playObj[String.fromCharCode(event.keyCode).toUpperCase()].play()
+  console.log(this.event.keyCode);
+  //console.log(document.getElementById(`${String.fromCharCode(this.event.keyCode).toUpperCase()}`)); //.parentElement.classList.toggle('pressedBtn');
+  const drumPad = document.getElementById(`${String.fromCharCode(this.event.keyCode).toUpperCase()}`).parentElement.classList
+  drumPad.toggle('pressedBtn');
+  setTimeout( ()=>  drumPad.toggle('pressedBtn') , 200);
+}
+// console.log(String.fromCharCode(event.keyCode).toUpperCase())
