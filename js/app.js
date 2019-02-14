d763e4ba23eb66c5ce5577eb234f1bb980d9ed9c
Shannan Kammann <sbkammann@gmail.com>
2019-02-13 18:22:38 -0800

    style: cleaned up the code

--- js/app.js
+++ js/app.js
@@ -1,16 +1,4 @@
-// User Story #5: When I click on a .drum-pad element, the audio clip contained in its child audio element should be triggered.
-// User Story #6: When I press the trigger key associated with each .drum-pad, the audio clip contained in its child audio element should be triggered (e.g. pressing the Q key should trigger the drum pad which contains the string "Q", pressing the W key should trigger the drum pad which contains the string "W", etc.).
-// User Story #7: When a .drum-pad is triggered, a string describing the associated audio clip is displayed as the inner text of the #display element (each string must be unique).
 
-// const Q = document.getElementById('Q');
-// const W = document.getElementById('W');
-// const E = document.getElementById('E');
-// const A = document.getElementById('A');
-// const S = document.getElementById('S');
-// const D = document.getElementById('D');
-// const Z = document.getElementById('Z');
-// const X = document.getElementById('X');
-// const C = document.getElementById('C');
 
 const display = document.getElementById('display');
 
@@ -55,7 +43,8 @@ const C = {
 const keyBox =  document.getElementById('keyBox');
 const playObj = {Q, W, E, A, S, D, Z, X, C};
 const drumPadArr = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'];
-keyBox.addEventListener('click', event => playObj[event.target.firstElementChild.id].audioClip.play()); //console.log(event.target.firstElementChild.id)
+keyBox.addEventListener('mousedown', buttonPress.bind(this, event)); //console.log(event.target.firstElementChild.id) playObj[event.target.firstElementChild.id].audioClip.play()
+keyBox.addEventListener('mouseup', buttonRelease.bind(this, event));
 window.addEventListener('keydown', buttonPress.bind(this, event) ); //playObj[String.fromCharCode(event.keyCode).toUpperCase()].play()
 window.addEventListener('keyup', buttonRelease.bind(this, event) )
 window.addEventListener('blur', removePressedBtnClass);
