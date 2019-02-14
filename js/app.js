235baecad22b159000d6b5f37baf214c7d995e02
Shannan Kammann <sbkammann@gmail.com>
2019-02-13 18:08:22 -0800

    feat: Added text display

--- js/app.js
+++ js/app.js
@@ -2,36 +2,79 @@
 // User Story #6: When I press the trigger key associated with each .drum-pad, the audio clip contained in its child audio element should be triggered (e.g. pressing the Q key should trigger the drum pad which contains the string "Q", pressing the W key should trigger the drum pad which contains the string "W", etc.).
 // User Story #7: When a .drum-pad is triggered, a string describing the associated audio clip is displayed as the inner text of the #display element (each string must be unique).
 
-const Q = document.getElementById('Q');
-const W = document.getElementById('W');
-const E = document.getElementById('E');
-const A = document.getElementById('A');
-const S = document.getElementById('S');
-const D = document.getElementById('D');
-const Z = document.getElementById('Z');
-const X = document.getElementById('X');
-const C = document.getElementById('C');
+// const Q = document.getElementById('Q');
+// const W = document.getElementById('W');
+// const E = document.getElementById('E');
+// const A = document.getElementById('A');
+// const S = document.getElementById('S');
+// const D = document.getElementById('D');
+// const Z = document.getElementById('Z');
+// const X = document.getElementById('X');
+// const C = document.getElementById('C');
+
+const display = document.getElementById('display');
+
+const  Q = {
+  audioClip: document.getElementById('Q'),
+  text: 'Heater-1'
+}
+const W = {
+  audioClip: document.getElementById('W'),
+  text: 'Heater-2'
+}
+const E = {
+  audioClip: document.getElementById('E'),
+  text: 'Heater-3'
+}
+const A = {
+  audioClip: document.getElementById('A'),
+  text: 'Heater-4'
+}
+const S = {
+  audioClip: document.getElementById('S'),
+  text: 'Clap'
+}
+const D = {
+  audioClip: document.getElementById('D'),
+  text: 'Open-HH'
+}
+const Z = {
+  audioClip: document.getElementById('Z'),
+  text: 'Kick-n\'-Hat'
+}
+const X = {
+  audioClip: document.getElementById('X'),
+  text: 'Kick'
+}
+const C = {
+  audioClip: document.getElementById('C'),
+  text: 'Closed-HH'
+}
+
 
 const keyBox =  document.getElementById('keyBox');
 const playObj = {Q, W, E, A, S, D, Z, X, C};
 const drumPadArr = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'];
-let isReleased = true;
-keyBox.addEventListener('click', event => playObj[event.target.firstElementChild.id].play()); //console.log(event.target.firstElementChild.id)
+keyBox.addEventListener('click', event => playObj[event.target.firstElementChild.id].audioClip.play()); //console.log(event.target.firstElementChild.id)
 window.addEventListener('keydown', buttonPress.bind(this, event) ); //playObj[String.fromCharCode(event.keyCode).toUpperCase()].play()
 window.addEventListener('keyup', buttonRelease.bind(this, event) )
 window.addEventListener('blur', removePressedBtnClass);
+
 function buttonPress(event){
+
   if(drumPadArr.includes(String.fromCharCode(this.event.keyCode).toUpperCase())){
-    playObj[String.fromCharCode(this.event.keyCode).toUpperCase()].play()
+    playObj[String.fromCharCode(this.event.keyCode).toUpperCase()].audioClip.play()
     const drumPad = document.getElementById(`${String.fromCharCode(this.event.keyCode).toUpperCase()}`).parentElement.classList;
     drumPad.toggle('pressedBtn');
-    isReleased = false;
+    display.classList.remove('displayNotUsed');
+    display.classList.add('displayInUse')
+    display.innerHTML = playObj[String.fromCharCode(this.event.keyCode).toUpperCase()].text;
   }
 }
 function buttonRelease(event){
 
   if(drumPadArr.includes(String.fromCharCode(this.event.keyCode).toUpperCase())){
-    console.log(String.fromCharCode(this.event.keyCode))
+    // console.log(String.fromCharCode(this.event.keyCode))
     const drumPad1 = document.getElementById(`${String.fromCharCode(this.event.keyCode).toUpperCase()}`).parentElement.classList;
     drumPad1.toggle('pressedBtn');
   }
